658222c91d5c0ba0b7d18c95328770c4053e79de
sajedaswad <you@example.com>
2023-06-04 12:38:47 +0300

    edit format

--- types/types.index.ts
+++ types/types.index.ts
@@ -1,16 +1,17 @@
 export namespace userNS {
+  export interface ITask {
+    taskId: string;
+    projectId: string;
+    projectName: string;
+    description: string;
+    beginTime: string;
+    endTime: string;
+    totalTaskTime: number;
+  }
   export interface IUserInfo {
     email: string;
     password: string;
     username: string;
   }
-  export interface ITask{
-    taskId:string;
-    projectId:string;
-    projectName:string;
-    description:string;
-    beginTime:string;
-    endTime:string;
-    totalTaskTime:number;
-    }
+
 }
