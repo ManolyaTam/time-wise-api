7404affd212ad0707fa77509ca4d1375b113be52
sajedaswad <you@example.com>
2023-06-13 13:02:57 +0300

    ...

--- controllers/sign-in-controller.ts
+++ controllers/sign-in-controller.ts
@@ -6,44 +6,14 @@ import { IUserInfo } from '../types/types.index';
 require('dotenv').config();
 
 const userSignInController = {
-  createUser: async (req: Request, res: Response) => {
-    try {
-      const { email, password } = req.body;
-
-      // Check if the email or password is missing
-      if (!email || !password) {
-        return res.status(400).json({ error: 'Email and password are required.' });
-      }
-
-      // Check if the email already exists in the database
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ error: 'Email already exists. Please use another email address.' });
-      }
-
-      // Create a new user instance
-      const newUser: IUserInfo = {
-        email,
-        password,
-      };
-
-      // Save the new user to the database
-      await User.create(newUser);
-
-      // Generate a JWT token for the new user
-      const token = generateToken(newUser);
-
-      res.status(201).json({ success: true, token });
-    } catch (error) {
-      console.error('Error occurred during sign up:', error);
-      res.status(500).json({ error });
-    }
-  },
 
   signIn: async (req: Request, res: Response) => {
     try {
       const { email, password } = req.body;
-
+      // Check if the email or password is missing
+      if (!email || !password) {
+        return res.status(400).json({ error: 'Email and password are required.' });
+      }
       // Check if the user exists in the database
       const user = await User.findOne({ email });
       if (!user) {
