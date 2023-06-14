9a0abc2c52be0d91f93c3fc715a0c3fc7fe8e659
sajeda swad <201047@ppu.edu.ps>
2023-06-15 00:09:16 +0200

    ########

--- controllers/project-controller.ts
+++ controllers/project-controller.ts
@@ -1,10 +1,71 @@
+// import { Request, Response } from 'express';
+// import { Project } from '../models/model.index';
+// import { IProject } from '../types/types.index';
+// import jwt, { JwtPayload } from 'jsonwebtoken';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const projectController = {
+//   createProject: async (req: Request, res: Response) => {
+//     const { name, color, projectHours, description, userEmail } = req.body;
+
+//     try {
+//       // Check if the project name already exists
+//       const existingProject = await Project.findOne({ name });
+
+//       if (existingProject) {
+//         return res.status(409).json({ error: 'Project name already exists' });
+//       }
+
+//       const projectData: IProject = {
+//         name,
+//         color,
+//         projectHours,
+//         description,
+//         userEmail,
+//       };
+
+//       const newProject = new Project(projectData);
+
+//       await newProject.save();
+//       res.status(201).json({ success: true });
+//     } catch (error) {
+//       console.log('error\n');
+//       console.log(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   },
+
+//   getAllProjects: async (req: Request, res: Response) => {
+//     const token = req.headers.authorization;
+
+//     try {
+//       // Verify and decode the token to get the user's email
+//       const decodedToken = jwt.verify(token, process.env.SECRET_KEY) as JwtPayload & { email: string };
+//       const userEmail = decodedToken.email;
+
+//       const projects = await Project.find({ userEmail }).select('name color');
+//       res.status(200).json({ success: true, projects });
+//     } catch (error) {
+//       console.log('error\n');
+//       console.log(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   }
+// };
+
+// export default projectController;
 import { Request, Response } from 'express';
 import { Project } from '../models/model.index';
 import { IProject } from '../types/types.index';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+require('dotenv').config();
 
 const projectController = {
   createProject: async (req: Request, res: Response) => {
-    const { name, color, projectHours, description, userEmail } = req.body;
+    const { name, color, projectHours, description } = req.body;
+    const token = req.body.token || req.query.token || req.headers['token']
 
     try {
       // Check if the project name already exists
@@ -13,6 +74,20 @@ const projectController = {
       if (existingProject) {
         return res.status(409).json({ error: 'Project name already exists' });
       }
+      // Check if the token is present
+      if (!token) {
+        return res.status(401).json({ error: 'Missing token' });
+      }
+      // Verify and decode the token to get the user's email
+      const decodedToken = jwt.verify(token, process.env.TOKEN_KEY || 'defaultSecretKey');
+
+      // Type guard to check if the decodedToken is not void
+      if (!decodedToken) {
+        return res.status(401).json({ error: 'Invalid token' });
+      }
+
+      // Type assertion to specify the type as { email: string }
+      const userEmail = (decodedToken as JwtPayload & { email: string }).email;
 
       const projectData: IProject = {
         name,
@@ -32,11 +107,26 @@ const projectController = {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   },
-
   getAllProjects: async (req: Request, res: Response) => {
-    const userEmail = req.body.userEmail;
+    const token = req.body.token || req.query.token || req.headers['token']
 
     try {
+      // Check if the token is present
+      if (!token) {
+        return res.status(401).json({ error: 'Missing token' });
+      }
+
+      // Verify and decode the token to get the user's email
+      const decodedToken = jwt.verify(token, process.env.TOKEN_KEY || 'defaultSecretKey');
+
+      // Type guard to check if the decodedToken is not void
+      if (!decodedToken) {
+        return res.status(401).json({ error: 'Invalid token' });
+      }
+
+      // Type assertion to specify the type as { email: string }
+      const userEmail = (decodedToken as JwtPayload & { email: string }).email;
+
       const projects = await Project.find({ userEmail }).select('name color');
       res.status(200).json({ success: true, projects });
     } catch (error) {
