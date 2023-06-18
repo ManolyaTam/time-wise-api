dc230d70745c3869091ad65b75ee0a7255ea16f3
sajeda swad <201047@ppu.edu.ps>
2023-06-18 08:20:09 +0200

    يا رب نعملو ميرج ونخلص منو

--- controllers/project-controller.ts
+++ controllers/project-controller.ts
@@ -1,116 +1,117 @@
 import { Request, Response } from 'express';
-import { Project, User } from '../models/model.index';
+import { User } from '../models/model.index';
 import { IProject } from '../types/types.index';
 import jwt, { JwtPayload } from 'jsonwebtoken';
-  import { ObjectId } from 'mongodb';
-
+import { ObjectId } from 'mongodb';
 require('dotenv').config();
 
 const projectController = {
   createProject: async (req: Request, res: Response) => {
     const { name, color, description } = req.body;
-  
+
     // Check if the required fields are provided
     if (!name || !color) {
       return res.status(400).json({ error: 'Name and color are required fields.' });
     }
-  
+
     let projectHours = 0;
     const token = req.body.token || req.query.token || req.headers['token'];
-  
+
     try {
       // Check if the token is present
       if (!token) {
         return res.status(401).json({ error: 'Missing token' });
       }
-  
+
       // Verify and decode the token to get the user's email
       const decodedToken = jwt.verify(token, process.env.TOKEN_KEY || 'defaultSecretKey');
-  
+
       // Type guard to check if the decodedToken is not void
       if (!decodedToken) {
         return res.status(401).json({ error: 'Invalid token' });
       }
-  
+
       // Type assertion to specify the type as { email: string }
       const userEmail = (decodedToken as JwtPayload & { email: string }).email;
-  
+
       // Find the user based on the email
       const user = await User.findOne({ email: userEmail });
-  
+
       if (!user) {
         return res.status(404).json({ error: 'User not found.' });
       }
-  
+
       // Check if the project name already exists for the current user
       const existingProject = user.projects.find((project: IProject) => project.name === name);
-  
+
       if (existingProject) {
         return res.status(409).json({ error: 'Project name already exists for the current user.' });
       }
-  
-      const projectData: Omit<IProject, '_id'> = {
+
+      const projectData: IProject = {
+        _id: new ObjectId(),
         name,
         color,
         projectHours,
         description,
         userEmail,
       };
-  
+
       user.projects.push(projectData); // Add project to user's projects array
-  
+
       await user.save();
-  
-      res.status(201).json({ success: true, project: projectData });
+
+      res.status(201).json({ success: true });
     } catch (error) {
       console.log('error\n');
       console.log(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-  }
-,
- 
+  },
   getAllProjectsData: async (req: Request, res: Response) => {
     const token = req.body.token || req.query.token || req.headers['token'];
-  
+
     try {
       // Check if the token is present
       if (!token) {
         return res.status(401).json({ error: 'Missing token' });
       }
-  
+
       // Verify and decode the token to get the user's email
       const decodedToken = jwt.verify(token, process.env.TOKEN_KEY || 'defaultSecretKey');
-  
+
       // Type guard to check if the decodedToken is not void
       if (!decodedToken) {
         return res.status(401).json({ error: 'Invalid token' });
       }
-  
+
       // Type assertion to specify the type as { email: string }
       const userEmail = (decodedToken as JwtPayload & { email: string }).email;
-  
+
       const user = await User.findOne({ email: userEmail });
-  
+
       if (!user) {
         return res.status(404).json({ error: 'User not found.' });
       }
-  
-      const projects = user.projects.map((project: IProject) => {
-        // Omit the userEmail field and include the id field
-        const { userEmail, _id, ...rest } = project;
-        return { id: _id, ...rest };
-      });
-  
+
+      let projects;
+      try {
+        projects = user.projects.map((project: IProject) => {
+          // Include the id field along with other project data
+          const { _id, ...rest } = project;
+          return { _id: _id ? _id.toString() : null, ...rest };
+        });
+      } catch (error) {
+        console.log('Error retrieving projects:', error);
+        return res.status(500).json({ error: 'Error retrieving projects' });
+      }
+
       res.status(200).json({ success: true, projects });
     } catch (error) {
-      console.log('error\n');
-      console.log(error);
+      console.log('Error:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-  }
-,  
-
+  },
   getAllProjectsNames: async (req: Request, res: Response) => {
     const token = req.body.token || req.query.token || req.headers['token'];
 
@@ -137,18 +138,18 @@ const projectController = {
         return res.status(404).json({ error: 'User not found.' });
       }
 
-      const projects = user.projects.map((project: { _id: string; name: string }) => ({
-        id: project._id,
+      const projects = user.projects.map((project: IProject) => ({
+        _id: project._id ? project._id.toString() : null,
         name: project.name
-      }))
+      }));
 
       res.status(200).json({ success: true, projects });
     } catch (error) {
-      console.log('error\n');
-      console.log(error);
+      console.log('Error:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   }
-};
 
+
+};
 export default projectController;
--- routes/project-routes.ts
+++ routes/project-routes.ts
@@ -6,9 +6,11 @@ const projectRouter = express.Router();
 // Create a new project
 projectRouter.post('/', projectController.createProject);
 
+//Get all project Data
+projectRouter.get('/cards', projectController.getAllProjectsData);
+
 //Get all project Name
 projectRouter.get('/list', projectController.getAllProjectsNames);
 
-//Get all project Data
-projectRouter.get('/cards', projectController.getAllProjectsData);
+
 export default projectRouter;
