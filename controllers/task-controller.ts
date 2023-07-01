e937359e39a37cc14c620378d290c05984bacfee
sajeda swad <201047@ppu.edu.ps>
2023-07-01 06:20:10 +0200

    finshe edit task

--- controllers/task-controller.ts
+++ controllers/task-controller.ts
@@ -168,9 +168,68 @@ const taskController = {
       console.log(error);
       res.status(500).json('Internal Server Error');
     }
+  },
+  updateTask: async (req: Request, res: Response) => {
+    const token = req.body.token || req.query.token || req.headers['token'];
+    const taskId = req.params.taskId;
+    const { description, beginTime, endTime } = req.body;
+
+    try {
+      // Check if the token is present
+      if (!token) {
+        return res.status(401).json({ error: 'Missing token' });
+      }
+
+      // Verify and decode the token to get the user's email
+      const decodedToken = jwt.verify(token, process.env.TOKEN_KEY || 'defaultSecretKey');
+
+      // Type guard to check if the decodedToken is not void
+      if (!decodedToken) {
+        return res.status(401).json({ error: 'Invalid token' });
+      }
+
+      // Type assertion to specify the type as { email: string }
+      const userEmail = (decodedToken as JwtPayload & { email: string }).email;
+
+      // Find the user based on the email
+      const user = await User.findOne({ email: userEmail });
+
+      if (!user) {
+        return res.status(404).json({ error: 'User not found.' });
+      }
+
+      // Find the task by ID
+      const task = user.tasks.find((task: ITask) => task._id.toString() === taskId);
+
+      if (!task) {
+        return res.status(404).json({ error: 'Task not found.' });
+      }
+
+      // Update the task properties
+      if (description) {
+        task.description = description;
+      }
+      if (beginTime) {
+        task.beginTime = beginTime;
+      }
+      if (endTime) {
+        task.endTime = endTime;
+      }
+
+      // Save the user to persist the changes
+      await user.save();
+
+      console.log("Updated Task:", task);
+      console.log("User Tasks After Update:", user.tasks);
+
+      res.status(200).json({ message: 'Task updated successfully.', task });
+    } catch (error) {
+      console.log('error\n');
+      console.log(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
 };
 
 export default taskController;
-
