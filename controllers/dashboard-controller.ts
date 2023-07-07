596c5242f58fcf55593413b338eb215064d9c4de
sajeda swad <201047@ppu.edu.ps>
2023-07-07 10:38:39 +0200

    finish endpiont barchart

--- controllers/dashboard-controller.ts
+++ controllers/dashboard-controller.ts
@@ -8,13 +8,7 @@ require('dotenv').config();
 const barChartController = {
   getBarChartData: async (req: Request, res: Response) => {
     const { startDate, endDate } = req.query;
-    console.log(startDate);
 
-    const daysInRange: number[] = [];
-    for (let tmpD = parseInt(startDate?.toString() || '0'); tmpD <= parseInt(endDate?.toString() || '0'); tmpD += (60 * 60 * 1000 * 24)) {
-      console.log(new Date(tmpD).toLocaleString());
-      daysInRange.push(tmpD)
-    }
     const token = req.body.token || req.query.token || req.headers['token'];
 
     try {
@@ -39,106 +33,83 @@ const barChartController = {
 
       if (!user) {
         return res.status(404).json({ error: 'User not found.' });
-      }// Filter tasks within the given date range for the user with status "stopped"
+      }
+
+      if (!startDate || !endDate) {
+        return res.status(400).json({ error: 'Missing start or end date' });
+      }
+      if (startDate >= endDate) {
+        return res.status(400).json({ error: 'invalid date' });
+
+      }
+
+      const startTimestamp = new Date(parseInt(startDate.toString()));
+      const endTimestamp = new Date(parseInt(endDate.toString()));
+
+      // Filter tasks within the given date range for the user with status "stopped"
       const stoppedTasks = user.tasks.filter((task: ITask) => {
-        const taskDate = task.beginTime;
-
-        if (!startDate || !endDate) {
-          return res.status(400).json({ error: 'Missing start or end date' });
-        }
-        if(startDate>=endDate){
-          return res.status(400).json({ error: 'Invalid Date' });
-        }
-
-        const startTimestamp = parseInt(startDate.toString());
-        const endTimestamp = parseInt(endDate.toString());
-
-        return taskDate >= startTimestamp && taskDate <= endTimestamp && task.status === "stopped";
-      });
-
-      // Group tasks by date
-      const tasksByDate: { [date: string]: { [project: string]: number } } = {};
-
-      stoppedTasks.forEach((task: ITask) => {
-        const project = user.projects.find((p: IProject) => p._id.toString() === task.projectId.toString()) || null;
-        console.log("projects", project)
-        if (!project) {
-          throw new Error(`Project not found for task with ID: ${task._id}`);
-        }
-
         const taskDate = new Date(parseInt(task.beginTime.toString()));
-
-        // Check if the parsed date is valid
-        if (isNaN(taskDate.getTime())) {
-          throw new Error(`Invalid date format for task with ID: ${task._id}`);
-
-        }
+        return taskDate >= startTimestamp && taskDate <= endTimestamp && task.status === 'stopped';
       });
 
-        // const obj = {
-        //   1: 2, 3: 4
-        // }
-        // Object.keys(obj).forEach(key => console.log(obj.key))
-        const timeline: { [date: string]: { [project: string]: number } }[] = [];
-        for (const day of daysInRange) {
-          console.log(day);
-          const projectData: { [project: string]: number } = {};
-          for (const task of stoppedTasks) {
-            const taskDate = new Date(parseInt(task.beginTime.toString()));
-            console.log(taskDate)
-            const taskDay = new Date(taskDate.setHours(0, 0, 0, 0));
-        
-            if (
-              taskDate.setHours(0, 0, 0, 0) === new Date(task.endTime).setHours(0, 0, 0, 0) &&
-              taskDay.getTime() === new Date(day).setHours(0, 0, 0, 0)
-            ) {
-              const duration = task.endTime - task.beginTime;
-              const project = user.projects.find((p: IProject) => p._id.toString() === task.projectId.toString()) || null;
-        
-              if (project) {
-                projectData[project.name] = (projectData[project.name] || 0) + duration;
-              }
 
+      // Create an array of all dates within the range
+      const dateRange: Date[] = [];
+      const currentDate = new Date(startTimestamp);
 
+      while (currentDate <= endTimestamp) {
+        dateRange.push(new Date(currentDate));
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+      const timeline: { [date: string]: { [project: string]: number } }[] = [];
 
+      // Loop through each date in the range
+      for (const date of dateRange) {
+        const formattedDate = date.toLocaleDateString();
 
+        const projectsData: { [project: string]: number } = {};
 
+        // Loop through each stopped task on the current date
+        for (const task of stoppedTasks) {
+          const taskDate = new Date(parseInt(task.beginTime.toString()));
 
-            // if (taskDate.setHours(0, 0, 0, 0) === new Date(task.endTime).setHours(0, 0, 0, 0)
-            //   && (new Date(day).setHours(0, 0, 0, 0)) === taskDate.setHours(0, 0, 0, 0)) {
-            //   let duration = task.endDate - task.beginTime;
-            //   const project = user.projects.find((p: IProject) => p._id.toString() === task.projectId.toString()) || null;
-            //   projectData[project.name] += duration;
+          if (taskDate.toLocaleDateString() === formattedDate) {
+            const project = user.projects.find((p: IProject) => p._id.toString() === task.projectId.toString()) || null;
 
-            // }
+            if (!project) {
+              throw new Error(`Project not found for task with ID: ${task._id}`);
+            }
 
-          }}
+            const duration = task.endTime - task.beginTime;
 
-          for (const [projectName, totalDuration] of Object.entries(projectData)) {
-            projectData[projectName] = totalDuration / (60 * 60 * 1000); // Convert to hours
+            if (projectsData[project.name]) {
+              projectsData[project.name] += duration;
+            } else {
+              projectsData[project.name] = duration;
+            }
           }
-      
-          timeline.push({ [day]: projectData });
         }
-        res.status(200).json({ success: true, timeline });
-      
 
-          //task starts and ends in this day
-          //if( task.start>= day && task.end <= day + (60*60*24*1000)){
-            // push timeline 
-            // timeline.push({ [day]: projectData });
+        // Convert project durations from milliseconds to hours
+        const projectHours: { [project: string]: number } = {};
 
-        // }
+        for (const [project, duration] of Object.entries(projectsData)) {
+          projectHours[project] = duration / 3600000; // Convert to hours
+        }
+
+        // Add the date and projects data to the timeline array
+        const timelineData: { [project: string]: number } = { ...projectHours };
+        //Add the date and projects data to the timeline array
+        timeline.push({ [formattedDate]: { ...timelineData } });
 
 
-  } catch(error) {
-    console.log('error\n');
-    console.log(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-},
+      }
+
+      res.status(200).json({ success: true, timeline });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
 };
-
-
 
 export default barChartController;
